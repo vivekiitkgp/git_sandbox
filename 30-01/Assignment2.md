bbce83ef5365ccea87c6c3a31ff39f18dd9f3bb8
Vivek Rai <vivekraiiitkgp@gmail.com>
2015-01-30 14:46:35 +0530

    Lab on 30th Jan

---
+++ 30-01/Assignment2.md
@@ -0,0 +1,11 @@
+Q. Calculate the amino acid composition of Asparagine Synthetase (PDB ID: 12AS)
+
+a. Percentage of hydrophobic (Ala, Gly, Val, Ile, Leu, Pro)
+
+b. Percentage of positively charged (Arg, Lys)
+
+c. Percentage of negatively charged (Asp, Glu)
+
+d. Percentage of neutral polar (Ser, Thr, His, Cys, Met, Asn, Gln)
+
+e. Percentage of aromatic (Phe, Tyr, Trp)
---
+++ 30-01/solution.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# vim:fileencoding=utf-8
+
+""" Calculates the number and percentage composition of different
+types of amino acid residues within a protein."""
+
+HYDROPHOBIC_AA = ['ALA', 'GLY', 'VAL', 'ILE', 'LEU', 'PRO']
+POSITIVE_AA    = ['ARG', 'LYS']
+NEGATIVE_AA    = ['ASP', 'GLU']
+NEUTRAL_AA     = ['SER', 'THR', 'HIS', 'CYS', 'MET', 'ASN', 'GLN']
+AROMATIC_AA    = ['PHE', 'TYR', 'TRP']
+
+def get_composition(pdb_file):
+    count = {
+            'TOTAL_AA'       : 0,
+            'HYDROPHOBIC_AA' : 0,
+            'POSITIVE_AA'    : 0,
+            'NEGATIVE_AA'    : 0,
+            'NEUTRAL_AA'     : 0,
+            'AROMATIC_AA'    : 0
+            }
+
+    for line in pdb_file.readlines():
+        if line.startswith('SEQRES'):
+            residues = line.split()[4:]
+            count['TOTAL_AA'] += len(residues)
+
+            for residue in residues:
+                if residue in HYDROPHOBIC_AA:
+                    count['HYDROPHOBIC_AA']  += 1
+                elif residue in POSITIVE_AA:
+                    count['POSITIVE_AA']     += 1
+                elif residue in NEGATIVE_AA:
+                    count['NEGATIVE_AA']     += 1
+                elif residue in NEUTRAL_AA:
+                    count['NEUTRAL_AA']      += 1
+                elif residue in AROMATIC_AA:
+                    count['AROMATIC_AA']     += 1
+
+    return count
+
+if __name__ == "__main__":
+    with open('12AS.pdb', 'r', encoding='utf-8') as f:
+        composition = get_composition(f)
+        total_aa    = composition.pop('TOTAL_AA')
+        print('{:15}: 660, 100%'.format('TOTAL_AA'))
+        for key, value in composition.items():
+            print("{:15}: {:3}/{}, {:.2f} %".format(key,
+                                                    value,
+                                                    total_aa,
+                                                    100*value/total_aa))
